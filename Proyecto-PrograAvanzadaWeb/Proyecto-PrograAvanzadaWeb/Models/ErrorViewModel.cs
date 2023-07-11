12c729ab258eab8590c619d045af40fb01e54035
Jant2515 <joseant.2515@gmail.com>
2023-07-11 15:24:47 -0600

    Creacion proyecto nuevo

---
+++ Proyecto-PrograAvanzadaWeb/Proyecto-PrograAvanzadaWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_PrograAvanzadaWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
