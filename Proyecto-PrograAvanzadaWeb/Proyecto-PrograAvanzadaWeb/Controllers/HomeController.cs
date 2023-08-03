23c77d662d74248887c6ca98934e4a50f98c389c
itsnicolee <mnicolealvarez3@gmail.com>
2023-08-03 14:09:48 -0600

    frontend home

--- Proyecto-PrograAvanzadaWeb/Proyecto-PrograAvanzadaWeb/Controllers/HomeController.cs
+++ Proyecto-PrograAvanzadaWeb/Proyecto-PrograAvanzadaWeb/Controllers/HomeController.cs
@@ -26,6 +26,10 @@ namespace Proyecto_PrograAvanzadaWeb.Controllers
         {
             return View();
         }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
